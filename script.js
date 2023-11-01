e466380c71bfb6e6848b0c261a5515e01f0a4262
deepika-reddy-b <149035324+deepika-reddy-b@users.noreply.github.com>
2023-11-01 12:00:35 +0530

    updated

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addTask() {
 
 function addlist(taskname,ul){
     var editbtn=document.createElement("button");
+    var delbtn=document.createElement("button");
     var li = document.createElement("li");
     var checkbox = document.createElement('input');
     var task = document.createElement("label");
@@ -31,11 +32,14 @@ function addlist(taskname,ul){
     editbtn.innerHTML="Edit";
     editbtn.className="edit "+taskname.value;
     editbtn.onclick=function () {
-        //console.log(task);
-        //task.hidden=true;
         editButton(this.parentNode);
     };
+    delbtn.innerHTML="Delete";
+    delbtn.onclick=function () {
+        this.parentNode.remove();
+    };
     li.appendChild(editbtn);
+    li.appendChild(delbtn);
     ul.appendChild(li);
     taskname.value = "";
 }
@@ -54,18 +58,18 @@ function markDone(idd) {
     if (checkBox.checked) {
         ulTasks.removeChild(liTask);
         ulCompleted.appendChild(liTask);
+        liTask.children[2].disabled=true;
     } else {
         ulCompleted.removeChild(liTask);
         ulTasks.appendChild(liTask);
+        liTask.children[2].disabled=false;
     }
 }
 
 function editButton(li){
-    //console.log(taskname);
     var label = li.querySelector('label');
     label.style.display = 'none';
-    console.log(document.getElementById(li.id).children[2]);
-    document.getElementById(li.id).children[2].style.disable=true;
+    document.getElementById(li.id).children[2].disabled=true;
     var input = document.createElement("input");
     input.setAttribute("type", "text");
     input.setAttribute("value", li.id);
@@ -79,6 +83,7 @@ function editButton(li){
                 label.innerHTML = tvalue;
                 label.style.display = 'inline';
                 input.remove();
+                document.getElementById(li.id).children[2].disabled=false;
             } else {
                 alert("Enter task Name");
             }
@@ -88,26 +93,5 @@ function editButton(li){
     // Make sure the input field is displayed
     input.style.display = 'inline';
 }
-//     var input="";
-//     var li = document.getElementById(taskname);
-//     //li.children[0].hidden = true; // checkbox
-//     li.children[1].hidden = true; // edit button
-//     var inp=document.createElement("input");
-//     inp.setAttribute("type", "text");
-//     inp.setAttribute("value", taskname);
-//     li.appendChild(inp);
-//     inp.addEventListener("keypress", function(event) {         // If the user presses the "Enter" key on the keyboard
-//         if (event.key === "Enter") {           // Cancel the default action, if needed
-//           event.preventDefault();
-//           var tvalue = inp.value.trim(); 
-//             if(tvalue!=="") {
-                
-//                 input=inp.value;
-//                 console.log(input);
-//             } else alert("Enter task Name");            // Trigger the button element with a click
-//                 //document.getElementById("myBtn").click();
-//         } }
-//     )
-//     if (input!=="") return input;
+
     
-// }
